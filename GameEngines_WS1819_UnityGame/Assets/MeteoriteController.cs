c1a662f6e25495c6b5b7cbf5160d5d11257f0a59
Unknown <adrian.sabrowski@hotmail.de>
2018-11-23 11:32:43 +0100

    Added Crater and Meteorite Prefabs

---
+++ GameEngines_WS1819_UnityGame/Assets/MeteoriteController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteoriteController : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject crater;
+    bool hasCollided;
+    ContactPoint collPos;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (transform.position.y < -20)
+        {
+            Destroy(this.gameObject);
+
+        }
+        else if (hasCollided)
+        {
+            Destroy(this.gameObject);
+            Instantiate(crater, collPos.point, Quaternion.identity);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        hasCollided = true;
+        collPos = collision.contacts[0];
+    }
+}
---
+++ GameEngines_WS1819_UnityGame/Assets/Scripts/SpawnController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject objectReference;
+    //private Vector3 throwForce = new Vector3(0, -18, 0);
+
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", 1f, 3);
+    }
+
+    void SpawnObject()
+    {
+
+        GameObject meteorite = Instantiate(objectReference, new Vector3(Random.Range(-6, 6), 20, 0), Quaternion.identity) as GameObject;
+        //meteorite.GetComponent<Rigidbody>().AddForce(throwForce, ForceMode.Impulse);
+
+    }
+}
