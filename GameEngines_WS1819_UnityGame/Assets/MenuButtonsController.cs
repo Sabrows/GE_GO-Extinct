23bfef78ed961a0193ec06965a31c1fef4b1358d
Unknown <adrian.sabrowski@hotmail.de>
2018-12-04 21:29:25 +0100

    Added Models and Scripts
    - TRex Model (TurboSquid)
    - Asteroid Model (TurboSquid)
    - MenuButtons Script
    - Tutorial LineHandler Script

---
+++ GameEngines_WS1819_UnityGame/Assets/MenuButtonsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonsController : MonoBehaviour
+{
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void LoadSettings()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
